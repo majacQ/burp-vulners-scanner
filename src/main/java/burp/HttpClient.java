b74b367b0e4ef2ba382e7fd65f2942510a7f8385
Jacquelyn Vinson <jacqvinson@gmail.com>
2022-01-30 20:52:46 -0800

    fix: license (#1)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * HttpAsyncClient
    
    * bmf + gui + proxy
    
    * RELEASE-1.0
    
    * Update README.md
    
    * RELEASE-1.1
    
    * RELEASE-1.2
    
    Co-authored-by: Vulners Team <isox@vulners.com>
    Co-authored-by: Ivan <vankyver@users.noreply.github.com>
    Co-authored-by: vankyver <vankyver007@gmail.com>
    Co-authored-by: vankyver <vankyver@vulners.com>

--- src/main/java/burp/HttpClient.java
+++ src/main/java/burp/HttpClient.java
@@ -1,35 +1,68 @@
 package burp;
 
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
-import org.apache.http.impl.nio.client.HttpAsyncClients;
-import org.apache.http.ssl.SSLContexts;
+import org.json.JSONObject;
 
-import javax.net.ssl.SSLContext;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-class HttpClient {
+public class HttpClient {
 
-    public static CloseableHttpAsyncClient createSSLClient() {
-        TrustStrategy acceptingTrustStrategy = new TrustStrategy() {
+    private static String VULNERS_API_HOST = "vulners.com";
+    private static String VULNERS_API_PATH = "/api/v3/burp/";
 
-            @Override
-            public boolean isTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
-                return true;
-            }
-        };
+    private final IBurpExtenderCallbacks callbacks;
+    private final IExtensionHelpers helpers;
+    private final BurpExtender burpExtender;
 
-        SSLContext sslContext = null;
-        try {
-            sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
-        } catch (Exception e) {
-            System.out.println("Could not create SSLContext");
+    HttpClient(IBurpExtenderCallbacks callbacks, IExtensionHelpers helpers, BurpExtender burpExtender) {
+        this.burpExtender = burpExtender;
+        this.callbacks = callbacks;
+        this.helpers = helpers;
+    }
+
+    public JSONObject get(String action, Map<String, String> params) {
+        List<String> headers = new ArrayList<>();
+        headers.add("POST " + VULNERS_API_PATH + action + "/ HTTP/1.1");
+        headers.add("Host: " + VULNERS_API_HOST);
+        headers.add("User-Agent: vulners-burpscanner-v-1.2");
+        headers.add("Content-type: application/json");
+
+        JSONObject jsonBody = new JSONObject();
+
+        if (burpExtender.getApiKey() != null) {
+            jsonBody = jsonBody.put("apiKey", burpExtender.getApiKey());
         }
 
-        return HttpAsyncClients.custom()
-                .setHostnameVerifier(SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER)
-                .setSSLContext(sslContext).build();
+        for (Map.Entry<String, String> p: params.entrySet()) {
+            jsonBody = jsonBody.put(p.getKey(), p.getValue());
+        }
+
+        byte[] request = helpers.buildHttpMessage(headers, helpers.stringToBytes(jsonBody.toString()));
+        byte[] response = callbacks.makeHttpRequest(VULNERS_API_HOST, 443, true, request);
+        return parseResponse(response);
     }
+
+    private JSONObject parseResponse(byte[] response) {
+        String responseString = helpers.bytesToString(response);
+        IResponseInfo iResponseInfo = helpers.analyzeResponse(response);
+        String jsonString = responseString.substring(iResponseInfo.getBodyOffset());
+
+        JSONObject object = new JSONObject(jsonString);
+
+        try {
+            if (object.getString("result").equals("OK")) {
+                return object.getJSONObject("data");
+            } else {
+                callbacks.printOutput("[DEBUG] not OK");
+                callbacks.printOutput(jsonString);
+                return object;
+            }
+        } catch (Exception e) {
+            callbacks.printError("[ERROR]");
+            callbacks.printError(jsonString);
+            return object;
+        }
+    }
+
 }
--- src/main/java/burp/gui/path/PathsTable.java
+++ src/main/java/burp/gui/path/PathsTable.java
@@ -8,7 +8,12 @@ public class PathsTable extends JTable {
     private final DefaultTableModel defaultModel;
 
     public PathsTable() {
-        DefaultTableModel model = new DefaultTableModel();
+        DefaultTableModel model = new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
         model.addColumn("Domain");
         model.addColumn("path");
         model.addColumn("CVSS Score");
@@ -16,6 +21,7 @@ public class PathsTable extends JTable {
 
         setModel(model);
         this.defaultModel = model;
+        this.setAutoCreateRowSorter(true);
     }
 
     public DefaultTableModel getDefaultModel() {
